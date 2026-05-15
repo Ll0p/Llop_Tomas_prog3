6e45f28ccf8dd5b387e972348e98e4db92385bc4
Tomas Llop <tomasignaciollop@gmail.com>
2026-05-15 19:10:30 -0300

    primer commit

---
+++ carta.js
@@ -0,0 +1,70 @@
+export class Carta {
+    code // string
+    value // string
+    suit // string
+    imagen // string (ES EL URL DE LA IMAGEN)
+
+    constructor(code, value, suit, imagen) {
+        this.code = code;
+        this.value = value;
+        this.suit = suit;
+        this.imagen = imagen;
+    }
+
+    toJsonString() {
+        return JSON.stringify(this);
+    }
+
+    static createFromJsonString(json) {
+        const datos = JSON.parse(json);
+        return new Carta(
+            datos.code,
+            datos.value,
+            datos.suit,
+            datos.imagen
+        );
+    }
+
+    createHtmlElement(){
+        const div = document.createElement("div");
+        const code = document.createElement("h3");
+        const value = document.createElement("p");
+        const suit = document.createAttribute("p");
+        const imagen = document.createElement("img");
+        const botonGuardado = document.createElement("button");
+
+        imagen.addEventListener("click", () => {
+            window.open(this.imagen, "_blank");
+        });
+
+        botonGuardado.addEventListener("click", () => {
+            Carta.guardarCarta(this);
+        });
+
+        div.className = "carta";
+        code.textContent = this.code;
+        value.textContent = this.value;
+        suit.textContent = this.suit;
+        imagen.src = this.imagen;
+        botonGuardado.textContent = "Guardar";
+
+        div.appendChild(code, imagen, value, suit, botonGuardado);
+        
+        return div;
+    }
+
+    static guardarCarta(carta) {
+        let cartas = JSON.parse(localStorage.getItem("cartas"));
+        if (cartas === null) cartas = [];
+
+        const existe = cartas.some(c => c.id === carta.id);
+
+        if (!existe) {
+            cartas.push(carta);
+            localStorage.setItem("cartas", JSON.stringify(cartas));
+        } else {
+            window.alert("La carta ya está en la colección.");
+        }
+    }
+
+}
