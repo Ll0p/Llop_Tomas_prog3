825a8484ed319cd774cfccb6917094564a71d077
Tomas Llop <tomasignaciollop@gmail.com>
2026-05-15 21:11:42 -0300

    Ajustando ordenamiento

--- logica/guardados.js
+++ logica/guardados.js
@@ -1,6 +1,22 @@
 import { Carta } from "./carta.js";
 import { crearCarta, meterCarta } from "./util_cartas.js"
 
+const ordenValores = {
+    "ACE": 14,
+    "KING": 13,
+    "QUEEN": 12,
+    "JACK": 11,
+    "10": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2
+};
+
 document.addEventListener("DOMContentLoaded", cargarGuardados);
 
 function cargarGuardados() {
@@ -20,7 +36,7 @@ window.ordenarPorValor = () => {
     let cartas = JSON.parse(localStorage.getItem("cartas"));
     if (cartas === null) cartas = [];
 
-    cartas.sort((a, b) => a.value.localeCompare(b.value));
+    cartas.sort((a, b) => ordenValores[a.value] - ordenValores[b.value]);
 
     localStorage.setItem("cartas", JSON.stringify(cartas));
 
